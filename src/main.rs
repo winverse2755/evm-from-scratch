93de416ac37df20f860574aad5a20871995acc55
winverse2755 <winverse2755@gmail.com>
2026-01-17 20:43:19 +0100

    pop test

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,13 @@ use mini_evm::Stack;
 fn main() {
     let mut stack = Stack::new();
 
-    stack.push(10);
-    stack.push(20);
-    stack.push(30);
+    stack.push(3);
+    stack.push(7);
+    stack.push(5);
     println!("{:?}", stack);
 
-    // println!("Popped: {:?}", stack.pop());
-    // println!("Popped: {:?}", stack.pop());
-    // println!("Popped: {:?}", stack.pop());
+    println!("Popped: {:?}", stack.pop());
+    println!("Popped: {:?}", stack.pop());
+    println!("Popped: {:?}", stack.pop());
+    println!("Popped: {:?}", stack.pop());
 }
