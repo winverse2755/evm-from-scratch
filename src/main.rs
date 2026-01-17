3556aaa494cd44d4a2b7958406b3217a23c2dc19
winverse2755 <winverse2755@gmail.com>
2026-01-17 20:45:54 +0100

    update main

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,15 @@ use mini_evm::Stack;
 fn main() {
     let mut stack = Stack::new();
 
+    //push items to stack and print
     stack.push(3);
     stack.push(7);
     stack.push(5);
     println!("{:?}", stack);
 
+    // pop one item
     println!("Popped: {:?}", stack.pop());
-    println!("Popped: {:?}", stack.pop());
-    println!("Popped: {:?}", stack.pop());
-    println!("Popped: {:?}", stack.pop());
+
+    //print stack
+    println!("{:?}", stack);
 }
