e1045adebc00f25522ce289e619817f9d87f6b76
winverse2755 <winverse2755@gmail.com>
2026-01-21 11:30:26 +0100

    math sub

--- src/opcodes/math.rs
+++ src/opcodes/math.rs
@@ -15,3 +15,11 @@ pub fn mul(evm: &mut EVM) {
     evm.gas_cost(5);
     evm.step();
 }
+
+pub fn sub(evm: &mut EVM) {
+    let a = evm.stack.pop().unwrap();
+    let b = evm.stack.pop().unwrap();
+    evm.stack.push(a - b);
+    evm.gas_cost(3);
+    evm.step();
+}
