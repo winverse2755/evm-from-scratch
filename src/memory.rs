103ebe6c4cf2dee8eede074990114d9a04d1d404
winverse2755 <winverse2755@gmail.com>
2026-01-21 12:58:15 +0100

    update memory

--- src/memory.rs
+++ src/memory.rs
@@ -1,3 +1,17 @@
 struct simple_memory {
     data: Vec<u32>,
 }
+
+impl simple_memory {
+    fn new() -> Self {
+        simple_memory { data: Vec::new() }
+    }
+
+    fn access(&mut self, offset: usize, size: usize) -> &[u32] {
+        let end = offset + size;
+        if end > self.data.len() {
+            self.data.resize(end, 0);
+        }
+        &self.data[offset..end]
+    }
+}
